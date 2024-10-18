6217fcfcf91c4ea1baca83eeb2f070de4a3ef9b2
nghlong <nghlong011@gmail.com>
2024-10-18 21:18:14 +0700

    [TEST]Font color

--- src/components/app.footer.tsx
+++ src/components/app.footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
                 className="footer_widget widget_jnews_about"
                 id="jnews_about-1"
               >
-                <h3 className="text-base font-bold uppercase">
+                <h3 className="text-base font-bold uppercase text-black">
                   <span>thesimpletrading.com</span>
                 </h3>
 
